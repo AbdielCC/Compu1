dd70b161489566c5430b8eb9a29e7a18b0e48bc0
Cabello Cardenas Ulises Abdiel <105956768+AbdielCC@users.noreply.github.com>
2024-08-13 14:42:31 -0600

    Add files via upload

---
+++ compu1 lab/circulo.java
@@ -0,0 +1,12 @@
+public class circulo{
+  public static void main(String[] args) {
+      int diametro=32;
+      final float pi =3.14f;
+      float area;
+      float perimetro;
+      area= pi * diametro;
+      perimetro= pi*((diametro/2)*(diametro/2));
+      System.out.println(area);
+      System.out.println(perimetro);
+  }
+}
---
+++ compu1 lab/paso.java
@@ -0,0 +1,7 @@
+public class paso{
+  public static void main(String[] args) {
+      float libra=2.204f;
+      float kilogramos=20;
+      System.out.println(libra*kilogramos);
+  }
+}
